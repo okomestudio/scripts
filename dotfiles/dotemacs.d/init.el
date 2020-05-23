5dbda6153c42a921af913c68cadd0dff8968fb51
Taro Sato <okomestudio@gmail.com>
2020-05-23 16:46:22 -0700

    Implement proper spell checking in JS code

--- dotfiles/dotemacs.d/init.el
+++ dotfiles/dotemacs.d/init.el
@@ -707,36 +707,48 @@
   (defun ts/web-mode-flyspell-verify ()
     ;; For detail, see:
     ;; http://blog.binchen.org/posts/effective-spell-check-in-emacs.html
-    (let* ((f (get-text-property (- (point) 1) 'face))
-           rlt)
-      (cond
-       ;; Check the words with these font faces, possibly.
-       ;; This *blacklist* will be tweaked in next condition.
-       ((not (memq f '(web-mode-html-attr-value-face
-                       web-mode-html-tag-face
-                       web-mode-html-attr-name-face
-                       web-mode-constant-face
-                       web-mode-doctype-face
-                       web-mode-keyword-face
-                       web-mode-comment-face  ;; focus on get html label right
-                       web-mode-function-name-face
-                       web-mode-variable-name-face
-                       web-mode-css-property-name-face
-                       web-mode-css-selector-face
-                       web-mode-css-color-face
-                       web-mode-type-face
-                       web-mode-block-control-face)))
-        (setq rlt t))
-       ;; Check attribute value under certain conditions:
-       ((memq f '(web-mode-html-attr-value-face))
-        (save-excursion
-          (search-backward-regexp "=['\"]" (line-beginning-position) t)
-          (backward-char)
-          (setq rlt (string-match "^\\(value\\|class\\|ng[A-Za-z0-9-]*\\)$"
-                                  (thing-at-point 'symbol)))))
-       ;; Finalize the blacklist.
-       (t (setq rlt nil)))
-      rlt))
+    (cond ((string= web-mode-content-type "html")
+           (let* ((f (get-text-property (- (point) 1) 'face))
+                  rlt)
+             (cond
+              ;; Check the words with these font faces, possibly.
+              ;; This *blacklist* will be tweaked in next condition.
+              ((not (memq f '(web-mode-html-attr-value-face
+                              web-mode-html-tag-face
+                              web-mode-html-attr-name-face
+                              web-mode-constant-face
+                              web-mode-doctype-face
+                              web-mode-keyword-face
+                              web-mode-comment-face  ;; focus on get html label right
+                              web-mode-function-name-face
+                              web-mode-variable-name-face
+                              web-mode-css-property-name-face
+                              web-mode-css-selector-face
+                              web-mode-css-color-face
+                              web-mode-type-face
+                              web-mode-block-control-face)))
+               (setq rlt t))
+              ;; Check attribute value under certain conditions:
+              ((memq f '(web-mode-html-attr-value-face))
+               (save-excursion
+                 (search-backward-regexp "=['\"]" (line-beginning-position) t)
+                 (backward-char)
+                 (setq rlt (string-match "^\\(value\\|class\\|ng[A-Za-z0-9-]*\\)$"
+                                         (thing-at-point 'symbol)))))
+              ;; Finalize the blacklist.
+              (t (setq rlt nil)))
+             rlt))
+          ((or (string= web-mode-content-type "javascript")
+               (string= web-mode-content-type "jsx"))
+           (let* ((f (get-text-property (- (point) 1) 'face)))
+             ;; *whitelist*
+             ;; only words with following font face will be checked
+             (memq f '(js2-function-call
+                       js2-function-param
+                       js2-object-property
+                       font-lock-variable-name-face
+                       font-lock-string-face
+                       font-lock-function-name-face))))))
 
   (put 'web-mode 'flyspell-mode-predicate 'ts/web-mode-flyspell-verify)
 
