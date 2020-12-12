37073430ff04fe0a98ccef901056b5e068b396ea
Taro Sato <okomestudio@gmail.com>
2020-12-12 11:48:54 -0800

    Reorder settings for projectile

--- dotconfig/emacs/init.el
+++ dotconfig/emacs/init.el
@@ -731,9 +731,9 @@ detail."
 
 (use-package projectile
   :config
+  (projectile-mode +1)
   (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
-  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
-  (projectile-mode +1))
+  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map))
 
 
 (use-package py-isort
