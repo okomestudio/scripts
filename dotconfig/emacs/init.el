29058fcb0fa12fed35a0c9901e38b770ac39637d
Taro Sato <okomestudio@gmail.com>
2021-03-07 21:00:48 -0800

    Add package-refresh-contents

--- dotconfig/emacs/init.el
+++ dotconfig/emacs/init.el
@@ -179,6 +179,7 @@ detail."
 (package-initialize)
 
 (unless (package-installed-p 'use-package)
+  (package-refresh-contents)
   (package-install 'use-package))
 (require 'use-package)
 (setq use-package-always-ensure t)
