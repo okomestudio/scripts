af66108e53157fc3de7b99fb1d144f83c3c862b3
Taro Sato <okomestudio@gmail.com>
2021-06-26 21:36:12 -0700

    Add google-translate

--- dotconfig/emacs/init.el
+++ dotconfig/emacs/init.el
@@ -1278,5 +1278,19 @@ detail."
   (tramp-default-method "ssh"))
 
 
+(use-package google-translate
+  :bind
+  (("C-c t" . 'google-translate-at-point)
+   ("C-c T" . 'google-translate-query-translate))
+
+  :config
+  (defun google-translate--search-tkk () "Search TKK." (list 430675 2721866130))
+
+  :custom
+  ((google-translate-backend-method 'curl)
+   (google-translate-default-source-language "auto")
+   (google-translate-default-target-language "ja")))
+
+
 (provide 'init)
 ;;; init.el ends here
